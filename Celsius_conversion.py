3967fcc1ead9876306ea7423c967d721baa8832f
Jeremy <jeremyw135@gmail.com>
2020-09-16 14:00:41 -0400

    C to F conversion

---
+++ Celsius_conversion.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    temp_in_c = float(input("What is the Celsius Temp:"))
+    temp_in_f= temp_in_c * 1.8 + 32
+    print(f"{temp_in_c} degrees Celsius is {temp_in_f} in Fahrenheit")
+
+
+main()
